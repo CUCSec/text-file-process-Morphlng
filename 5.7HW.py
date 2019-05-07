8de23bd432bd920e308ac34a0c4a970473c1714a
David <1193115470@qq.com>
2019-05-07 22:15:49 +0800

    安哥辛苦了，以下请忽略 上传玩玩。

---
+++ 5.7HW.py
@@ -0,0 +1,26 @@
+﻿names=[]
+
+DicCount={}
+
+file_name='201811113001.log'
+
+studentID=file_name.split('.')[0]
+
+with open(file_name,encoding='utf-8') as f:
+    for line in f:
+        student_id = line.split(',')[1]
+        student_id = student_id.strip()
+        names.append(student_id)
+
+names.sort()
+
+
+for name in names:
+     DicCount[name]=names.count(name)
+
+print("The whole attendence records:")
+print(DicCount)
+
+print("Your attendence:")
+print(DicCount["学号："+studentID])
+
